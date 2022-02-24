cb251d4eaa320045feb664c0fcfe07725b94ebf9
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2022-02-24 15:49:53 -0600

    Ya está suma. Faltan detalles de resta y producto

--- Practica01/src/parte1.hs
+++ Practica01/src/parte1.hs
@@ -29,21 +29,74 @@ suma U (Cero x) = Uno x
 suma (Uno x) U = Cero (suma U x)
 suma U (Uno x) = Cero (suma U x)
 
-suma (Cero x) (Cero y) = suma x y
+suma (Cero x) (Cero y) = Cero (suma x y)
 suma (Uno x) (Uno y) = Cero (suma (suma x U) y)
 suma (Uno x) (Cero y) = Uno (suma x y)
 suma (Cero x) (Uno y) = Uno (suma x y)
---suma = error "falta implementar"
+ 
 
 -- Función que devuelve su resta
 resta :: BinPos -> BinPos -> BinPos
-resta = error "falta implementar"
+resta U U = U --pk no hay cero
+ 
+resta (Cero x) U = Uno (resta x U)-- 110 - 1 = 101  Uno ((resta x U) x)
+                -- dos casos 
+
+resta U (Cero x) = U
+resta (Uno x) U = Cero x
+resta U (Uno x) = U
+
+{- 
+1   1   
+2   10
+3   11
+4   100
+5   101
+6   110
+7   1 1 1
+8   1 00 0
+9   1001
+10  1 01 0
+11  1 01 1
+12  1100
+13  1101
+14  1 11 0
+15  1 11 1
+16  1 000 0
+-}
+
+resta (Cero x) (Cero y) = Cero (resta x y)
+resta (Uno x) (Uno y) = Cero (resta x y)
+resta (Uno x) (Cero y) = Uno (resta x y)
+resta (Cero x) (Uno y) = U
+--resta = error "falta implementar"
+
+{-
+restas :: Peano -> Peano -> Peano
+restas m Zero = m
+restas Zero m = restas m Zero
+restas (Succ m) (Succ Zero) = m 
+restas n m =  restas (restas n (Succ Zero)) (restas m (Succ Zero))-}
 
 -- Función que devuelve su producto
 producto :: BinPos -> BinPos -> BinPos
-producto = error "falta implementar"
+producto U U  = U
 
--- Función que dado un BinPos devuelve el número natural
+producto U (Cero x) = Cero x    --zzz0 1
+producto U (Uno x) = Uno x  -- zzz1 1
+producto (Uno x) U = Uno x -- zzz1  1
+producto (Cero x) U = Cero x
+
+producto (Cero x) (Uno y) = suma (Cero x) (Cero (producto (Cero x) y))  -- zzz0  zzz1   
+producto (Uno x) (Cero y) = suma (Cero y)  (Cero (producto (Cero x) y)) -- zzz1  zzz0
+producto (Cero x) (Cero y) = Cero (suma (Cero x) y)
+producto (Uno x) (Uno y) = suma x (Cero y)
+
+
+
+--producto = error "falta implementar"
+
+-- Función que dado un BinPos devuelve el número natural 
 binPosToInt :: BinPos -> Int
 binPosToInt x = binPosToIntAux x 0
 
@@ -58,8 +111,16 @@ intToBinPos :: Int -> BinPos
 intToBinPos 1 = U
 intToBinPos x = if mod x 2 == 0 then Cero (intToBinPos (div x 2)) else Uno (intToBinPos (div x 2)) 
 
+
+uno = U
 dos = Cero U  -- 10
 tres = Uno U  -- 11
 cuatro = Cero (Cero U)-- 100
 cinco = Uno (Cero U) -- 101
 seis = Cero (Uno U) -- 110
+siete = Uno (Uno U) -- 111
+ocho = Cero (Cero (Cero U))  -- 1000
+nueve = Uno (Cero (Cero U)) -- 1001
+diez = Cero (Uno (Cero U)) -- 1010
+
+cientoVeinticuatro = Cero(Cero (Uno (Uno (Uno (Uno U)))))
--- Practica01/src/parte2.hs
+++ Practica01/src/parte2.hs
@@ -23,21 +23,49 @@ data Balcanes = Albania
 type Ady = [(Balcanes,Balcanes)]
 
 adyacencias :: Ady
-adyacencias = [(Albania, Montenegro),(Albania ,Kosovo),(Albania,Macedonia),(Bulgaria , Macedonia),(BosniaYHerzegovina,Montenegro),(Kosovo,Macedonia),(Kosovo,Montenegro)]
+adyacencias = [(Albania, Montenegro),(Albania ,Kosovo),(Albania,Macedonia),(Bulgaria , Macedonia),
+                (BosniaYHerzegovina,Montenegro),(Kosovo,Macedonia),(Kosovo,Montenegro)]
 
 -- x es adyacente a y si (x,y) elem adyacencias ó si (y,x) elem adyacencias
 
--- Coloración. Esta relaciona a un color, con un país
+-- Coloración. Esta relaciona a un color, con un país, es una lista de tuplas
 type Coloracion = [(Color,Balcanes)]
 
+-- nosotros debemos probar estas xd
+colores :: Coloracion
+colores = [(Rojo, Montenegro),(Azul, Albania),(Verde,Bulgaria),(Amarillo,BosniaYHerzegovina),
+            (Rojo,Kosovo),(Verde, Macedonia)]
+
 -- Regresa si la coloración recibida es buena respecto a la matriz de adyacencias
+-- Ejemplo de una buena coloracion: [(Montenegro,Bulgaria)]
+-- Ejemplo de una mala coloracion: [(Macedonia,Bulgaria)]
+
 esBuena :: Ady -> Coloracion -> Bool 
-esBuena = error "Falta implentar"
+esBuena = error  "Falta implentar"
+{-esBuena ady colores =
+    let 
+        col = [[snd x | x <- colores, fst x == Rojo],
+               [snd x | x <- colores, fst x == Amarillo],
+               [snd x | x <- colores, fst x == Verde],
+               [snd x | x <- colores, fst x == Azul]] 
+               
+    in 
+        filter ((> 1) . length) col
+               
+    verifica :: Ady -> [(Balcanes, Balcanes)] -> Bool
+    verifica ady [] = True
+    verifica ady (x:xs)
+        | elem x ady = False
+        | otherwise = verifica ady xs
+    -}
 
 -- Calcula todas las coloraciones buenas y completas respecto a la matriz de adyacencias recibida
 coloraciones :: Ady -> [Coloracion]
 coloraciones = error  "Falta implentar"
-
-
-
-
+-- 
+{-coloraciones ady colores =  - }
+-- En este metodo, se tiene que usar el resultado obtenido en el anterior
+por ejemplo 
+coloracions = x+2 esBueno ady xd 
+creoooo
+-}
