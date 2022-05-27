c5b6bfa1a4438412e8a8257a8049a51a952fe8a9
Erick B <erick07@ciencias.unam.mx>
2022-05-26 19:36:38 -0500

    avances

--- Practica05/src/parte1.pl
+++ Practica05/src/parte1.pl
@@ -11,3 +11,28 @@ trib(1,0).
 trib(2,0).
 trib(3,1).
 trib(N,X) :- N > 1, trib(N-3,X1),trib(n-2,X2),trib(n-1,X3), X is X1+X2+X3.	
+
+
+%defincion de fact
+%fact 0 = 1
+%fact 1 = 1
+%fact 2 = 2
+%fact 3 = 6
+%fact 4 = 24
+%fact 5 = 120
+%asi
+
+fact(0,0).
+fact(1,0).
+fact(N,X) :- N>1, fact(N-1, XS), X is N*XS.
+
+%Lo de suma
+%suma 0 = 0
+%suma 1 = 1
+%suma 2 = 3
+%suma 3 = 6
+%suma 4 = 10
+%suma 5 = 15
+suma(0,0).
+suma(1,1).
+suma(N,X) :- N>1, suma(N-1, XS), X is N+XS.
