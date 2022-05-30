028531398fe3e76bf693da3b3c48f88733c76b46
Erick B <erick07@ciencias.unam.mx>
2022-05-30 15:32:21 -0500

    p1

--- Practica05/src/parte1.pl
+++ Practica05/src/parte1.pl
@@ -1,57 +1,90 @@
-
+/* 1. suseccion de tribonacci
 %definicion "Informal" de trib:
 %trib 0 = 0
 %trib 1 = 0
 %trib 2 = 0
 %trib 3 = 1
-%trib n = trib(n-1)+trib(n-2)+ trib(n-3) 
+%trib n = trib(n-1)+trib(n-2)+ trib(n-3)*/
 
 trib(0,0).
 trib(1,0).
 trib(2,0).
 trib(3,1).
-trib(N,X) :- N > 1, trib(N-3,X1),trib(n-2,X2),trib(n-1,X3), X is X1+X2+X3.	
+trib(N,X) :- N > 2, N3 is N-3, trib(N3,X1), N2 is N-2, trib(N2,X2), N1 is N-1, trib(N1,X3), X is X1+X2+X3.	
 
 
+/* 2. factorial
 %defincion de fact
 %fact 0 = 1
 %fact 1 = 1
 %fact 2 = 2
-%fact 3 = 6
-%fact 4 = 24
-%fact 5 = 120
-%asi
+%fact 3 = 6*/
 
-fact(0,0).
-fact(1,0).
-fact(N,X) :- N>1, fact(N-1, XS), X is N*XS.
+fact(0,1).
+fact(1,1).
+fact(N,X) :- N > 1, N1 is N-1, fact(N1,XS), X is N * XS.
 
-%Lo de suma
-%suma 0 = 0
-%suma 1 = 1
-%suma 2 = 3
-%suma 3 = 6
-%suma 4 = 10
-%suma 5 = 15
-suma(0,0).
-suma(1,1).
-suma(N,X) :- N>1, suma(N-1, XS), X is N+XS.
+factñ(0,1).
+factñ(1,1).
+factñ(N,X) :- N > 1, factñ(N1, XS), N1 is N-1, X is N*XS.
+/*comentario multiliena
+Aprendiendo prolog xd
+Al parecer el orden de las comas es diferente, no es lo mismo 
+fact(N,X)  :- N > 1, fact(N1, XS), N1 is N-1, X is N*XS.
+Así factñ(2,4) nos manda un error xd
+que 
+factñ(N,X) :- N > 1, N1 is N - 1, factñ(N1,X1), X is N * X1.
+así fact(2,4) nos da false, lo cual tiene sentido pues 4 no es el factorial de 2.
+La correcta es fact, sin ñ.
+*/
 
 
-% Ps lo binarios xd
+/* 3. suma los primeros n numeros
+%cuenta 0 = 0
+%cuenta 1 = 1
+%cuenta 2 = 3
+%cuenta 3 = 6
+%cuenta 4 = 10
+%cuenta 5 = 15*/
+cuenta(0,0).
+cuenta(1,1).
+cuenta(N,X) :- N>1, N1 is N-1, suma(N1, XS), X is N+XS.
+
+
+/* 4. suma todos los elementos de una lista
+%suma [1,2,3] = 6
+%suma [2,5,9] = 16
+%suma [0,5,2] = 7
+%suma [3,2,9] = 14*/
+suma([],0).
+suma([X|XS],N) :- suma(XS, N1), N is N1+X.
+
+/* 5. circuitos
+% Ps lo binarios xd*/
 % Or
-or(0,0,0).
-or(1,0,1).
-or(0,1,1).
-or(1,1,1).
+or(false,false,false).
+or(true,false,true).
+or(false,true,true).
+or(true,true,true).
 
 % And
-and(0,0,0).
-and(1,0,0).
-and(0,1,0).
-amd(1,1,1).
+and(false,false,false).
+and(true,false,false).
+and(false,true,false).
+amd(true,true,true).
 
 %Negacion
-not(1,0).
-not(0,1).
+not(true,false).
+not(false,true).
 
+
+circuitoa(X,Y,R) :- not(X,P), %negamos a X como P 
+                    and(P,Y,Q), %conjuncion de la negacion de X y Y como Q
+                    not(Q,S), %negacion del resultado anterior
+                    and(S,Y,R). %conjuncion de la negacion anterior y Y.
+
+circuitob(X,Y,Z,R) :- not(X,P), %negamos a X como P
+                      and(X,P,Q), %conjuncion de X y su negacion como Q
+                      not(Q,S), %volvemos a negar xd
+                      and(Y,Z,T), %conjuncion de Y y Z
+                      and(S,T,R). %conjuncion de los dos resultados anteriores
